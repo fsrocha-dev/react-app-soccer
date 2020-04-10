1f07ae1192ae8c1dc1521533822d2e7d24a38f2b
fsrocha-dev <contato.frankrocha@gmail.com>
2020-04-10 14:50:47 -0300

    criando componente base app e css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-console.log(process.env.REACT_APP_API_KEY);
-ReactDOM.render(<div>oi</div>, document.getElementById("root"));
+
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById("root"));
