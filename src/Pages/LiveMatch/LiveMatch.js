ddc8e8b6aa366eb8cf808b09644791c0296265c2
fsrocha-dev <contato.frankrocha@gmail.com>
2020-04-11 13:58:10 -0300

    criar placar dinamico do jogo ao vivo

--- src/Pages/LiveMatch/LiveMatch.js
+++ src/Pages/LiveMatch/LiveMatch.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { getMatch } from '../../services/api'
 
 import './LiveMatch.css'
 
 const LiveMatch = () => {
+  const [loading, setLoading] = React.useState(false)
+  const [match, setMatch] = React.useState()
+
+  React.useEffect(() => {
+    const load = async () => {
+      setLoading(true)
+      const match = await getMatch(1)
+      setMatch(match)
+      setLoading(false)
+    }
+    load()
+  }, [])
+
   return (
-    <div>Ao Vivo</div>
+    <div>
+      {loading && <div>Carregando...</div>}
+      {!loading && match && (
+        <div className="match" style={{ marginTop: '30px' }}>
+          <div className="match-header">
+            <div className="home-team">
+              <span className="team-name">{match.homeTeamName}</span>
+              <span className="team-score">0</span>
+            </div>
+            <div className="versus">X</div>
+            <div className="away-team">
+              <span className="team-score">0</span>
+              <span className="team-name">{match.awayTeamName}</span>
+            </div>
+          </div>
+          <div className="match-info">
+            <div className="match-date">01/01/2020</div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
