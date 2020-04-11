ee769ce1d6f879e49be54e8194f513c50504a57b
fsrocha-dev <contato.frankrocha@gmail.com>
2020-04-11 10:19:56 -0300

    criaçao logo tipo da home

--- src/Template/Logo/Logo.js
+++ src/Template/Logo/Logo.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Logo.css'
+import urlBola from '../../static/bola.png'
+
+const height = "2em"
 
 const Logo = () => (
   <div className="home-logo">
-    Logo
-  </div>
+    <div className="logo">
+      <img src={urlBola} alt="Logo Soccer" style={{ height }} />
+      <h3 style={{ lineHeight: height, fontSize: height }}>
+        <strong>Brazil</strong> Soccer
+    </h3>
+    </div>
+  </div >
 )
 
 export default Logo
