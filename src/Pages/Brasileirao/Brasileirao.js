5fb26f193c831f5cd4508af21186b559bf040eaf
fsrocha-dev <contato.frankrocha@gmail.com>
2020-04-11 10:48:21 -0300

    criar chamada a api

--- src/Pages/Brasileirao/Brasileirao.js
+++ src/Pages/Brasileirao/Brasileirao.js
@@ -1,10 +1,40 @@
 import React from 'react'
-
 import './Brasileirao.css'
+import { getLeagueTable, getNextMatches } from '../../services/api'
 
 const Brasileirao = () => {
+  const [loading, setLoading] = React.useState(false)
+  const [leagueTable, setLeagueTable] = React.useState()
+  const [nextMatches, setNextMatches] = React.useState()
+
+  React.useEffect(() => {
+    const leagueId = 2013
+    const load = async () => {
+      setLoading(true)
+      const promises = [
+        getLeagueTable(leagueId),
+        getNextMatches(leagueId)
+      ]
+      const [leagueTable, nextMatches] = await Promise.all(promises)
+      setLeagueTable(leagueTable.standings[0].table)
+      setNextMatches(nextMatches.matches)
+      setLoading(false)
+    }
+    load()
+  }, [])
+
   return (
-    <div>Brasileirao</div>
+    <div>
+      {loading && <h1>Carregando</h1>}
+      {!loading && (
+        <>
+          <h1>Tabela</h1>
+          <div></div>
+          <h1>Próximas Partidas</h1>
+          <div></div>
+        </>
+      )}
+    </div>
   )
 }
 
